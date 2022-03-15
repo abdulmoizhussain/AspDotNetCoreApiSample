95a9d3c305c62c8a83e4daf1f348907890094368
Abdul Moiz <moizhus123@gmail.com>
2022-03-15 23:30:54 +0500

    Written ResponseFilterAttribute : ActionFilterAttribute.

---
+++ WebApiNC/Attributes/ResponseFilterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+using WebApiNC.ResponseModels;
+
+namespace WebApiNC.Attributes
+{
+  public class ResponseFilterAttribute : ActionFilterAttribute
+  {
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+      if (context.Result is Microsoft.AspNetCore.Mvc.BadRequestObjectResult badRequestObject)
+      {
+        if (badRequestObject.Value is Microsoft.AspNetCore.Mvc.ValidationProblemDetails validationDetails)
+        {
+          var errors = new List<string>();
+
+          foreach (KeyValuePair<string, string[]> error in validationDetails.Errors)
+          {
+            foreach (string singleError in error.Value)
+            {
+              errors.Add(error.Key + ": " + singleError);
+            }
+          }
+
+          var result = new ApiResponse { Errors = errors };
+
+          context.Result = new Microsoft.AspNetCore.Mvc.BadRequestObjectResult(result);
+        }
+      }
+      base.OnResultExecuting(context);
+    }
+  }
+}
---
+++ WebApiNC/ResponseModels/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApiNC.ResponseModels
+{
+  public class ApiResponse
+  {
+    public object Data { get; set; }
+    public string Message { get; set; }
+    public IEnumerable<string> Errors { get; set; }
+
+
+    public static BadRequestObjectResult BadRequest(string errorMessage)
+    {
+      return new(new ApiResponse { Errors = new[] { errorMessage } });
+    }
+  }
+}
